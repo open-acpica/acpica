0d89ff626a75da897094efaf3b6c3d5cdf29d244
Saket Dumbre <saket.dumbre@intel.com>
2025-12-11 17:12:17 -0800

    Add complete support for the new ACPI table TPMC and verify the template generated .asl and .dsl files match exactly
    Signed-off-by: Saket Dumbre <saket.dumbre@intel.com>

--- source/common/dmtable.c
+++ source/common/dmtable.c
@@ -2187,7 +2187,7 @@ AcpiDmDumpTable (
 
         case ACPI_DMT_TPMC_PFS:
 
-            /* TPMC Start Method types */
+            /* TPMC PFS Start Method types */
 
             Temp8 = *Target;
             if (Temp8 > ACPI_TPMC_PFS_RESERVED)
